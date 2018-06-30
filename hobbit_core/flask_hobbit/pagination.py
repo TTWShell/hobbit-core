b932083c1314d6dcec0361a7bfa748958d7202b4
Legolas Bloom <zhanhsw@gmail.com>
2018-07-01 06:19:32 +0800

    add pagination

---
+++ hobbit_core/flask_hobbit/pagination.py
@@ -0,0 +1,56 @@
+from collections import Mapping
+from flask_sqlalchemy import model
+
+from marshmallow import fields
+
+
+class ParamsDict(dict):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def update(self, other=None):
+        ret = self.copy()
+        if other is not None:
+            for k, v in other.items() if isinstance(other, Mapping) else other:
+                ret[k] = v
+        return ret
+
+
+PageParams = ParamsDict(
+    page=fields.Int(missing=1, required=False),
+    page_size=fields.Int(missing=10, required=False),
+    order_by=fields.Str(missing='id', required=False),
+)
+
+
+def pagination(obj, page, page_size, order_by='id', query_exp=None):
+    if not isinstance(obj, model.DefaultMeta):
+        raise Exception('first arg obj must be model.')
+
+    if not isinstance(order_by, list):
+        order_by = [order_by]
+
+    columns = {i.name for i in obj.__table__.columns}
+    diff = {c.lstrip('-') for c in order_by} - columns
+    if diff:
+        raise Exception('columns {} not exist in {} model'.format(diff, obj))
+
+    order_exp = []
+    for column in order_by:
+        if column.startswith('-'):
+            order_exp.append(getattr(obj, column.lstrip('-')).desc())
+        else:
+            order_exp.append(getattr(obj, column))
+
+    qexp = query_exp or getattr(obj, 'query')
+
+    items = qexp.order_by(*order_exp).paginate(
+        page, page_size, error_out=False)
+
+    class Result:
+        items = items.items
+        page = page
+        page_size = page_size
+        total = items.total
+
+    return Result
