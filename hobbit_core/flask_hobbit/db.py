95c590720d2d2ad23edbd7a41161ec66aaff8c6f
Legolas Bloom <zhanhsw@gmail.com>
2018-12-07 18:06:18 +0800

    add index for created_at、updated_at cloumn

--- hobbit_core/flask_hobbit/db.py
+++ hobbit_core/flask_hobbit/db.py
@@ -21,9 +21,9 @@ class SurrogatePK(object):
 
     id = Column(Integer, primary_key=True)
     created_at = Column(
-        DateTime, nullable=False, server_default=func.now())
+        DateTime, index=True, nullable=False, server_default=func.now())
     updated_at = Column(
-        DateTime, nullable=False, server_default=func.now(),
+        DateTime, index=True, nullable=False, server_default=func.now(),
         onupdate=func.now())
 
     def __repr__(self):
