0e1084f6640103d9fbfdec84dcb3fa5614ea95cd
Legolas Bloom <zhanhsw@gmail.com>
2019-07-25 16:06:53 +0800

    refactor hobbit new cmd test and fix celery requirements.txt bug

--- tests/test_hobbit.py
+++ tests/test_hobbit.py
@@ -1,10 +1,12 @@
 import os
-from subprocess import call
+import subprocess
+import itertools
 
 import pytest
 from click.testing import CliRunner
 
 from hobbit import main as hobbit
+from hobbit.bootstrap import templates
 
 from . import BaseTest, rmdir, chdir
 
@@ -23,90 +25,60 @@ class TestHobbit(BaseTest):
     def runner(self):
         yield CliRunner()
 
-    def test_hobbit_cmd(self, runner):
+    def test_not_exist_cmd(self, runner):
         result = runner.invoke(hobbit)
         assert result.exit_code == 0
 
         result = runner.invoke(hobbit, ['doesnotexistcmd'], obj={})
         assert 'Error: cmd not exist: doesnotexistcmd' in result.output
 
+    @pytest.mark.parametrize(
+        'name,template,celery_,dist',
+        itertools.product(
+            ['haha'], templates, ['--celery', '--no-celery'],
+            [None, '.', wkdir]))
     @chdir(wkdir)
-    def test_new_cmd_nodist(self, runner):
-        assert os.getcwd() == self.wkdir
+    def test_new_cmd(self, runner, name, template, celery_, dist):
+        options = [
+            '--echo', 'new', '-p 1024', '-n', name,  '-t', template, celery_]
+        if dist:
+            assert os.getcwd() == os.path.abspath(dist)
+            options.extend(['-d', dist])
 
-        result = runner.invoke(hobbit, ['--echo', 'new'], obj={})
-        assert result.exit_code == 2
-        assert 'Error: Missing option "-n" / "--name".' in result.output
-
-        result = runner.invoke(
-            hobbit, [
-                '--echo', 'new', '-n', 'haha', '-f', '-p', '1024',
-            ], obj={})
-        assert result.exit_code == 0
+        result = runner.invoke(hobbit, options, obj={})
+        assert result.exit_code == 0, result.output
         assert 'mkdir\t{}'.format(self.wkdir) in result.output
         assert 'render\t{}'.format(self.wkdir) in result.output
-        assert os.path.exists(os.path.join(
-            os.getcwd(), 'app', 'models', '__init__.py'))
-        assert 'example.py' not in result.output
+        if template == 'shire':
+            if celery_ == '--no-celery':
+                # start + 29 files + 11 dir + 1 end + empty
+                # in this test case. main dir exists, so mkdir - 1
+                assert len(result.output.split('\n')) == \
+                    1 + 29 + 11 + 1 + 1 - 1
+            else:
+                # start + files + mkdir + tail
+                assert len(result.output.split('\n')) == 1 + 30 + 12 + 1
+        else:
+            if celery_ == '--no-celery':
+                assert len(result.output.split('\n')) == \
+                    1 + 31 + 11 + 1
+            else:
+                assert len(result.output.split('\n')) == 1 + 32 + 12 + 1
 
-    def test_new_cmd_dist(self, runner):
-        result = runner.invoke(
-            hobbit, [
-                '--echo', 'new', '-n', 'haha', '-f', '-d', self.wkdir,
-                '-p', '1024',
-            ], obj={})
-        assert result.exit_code == 0
-        assert 'mkdir\t{}'.format(self.wkdir) in result.output
-        assert 'render\t{}'.format(self.wkdir) in result.output
-        assert os.path.exists(os.path.join(
-            self.wkdir, 'app', 'models', '__init__.py'))
-        assert 'example.py' not in result.output
+        assert subprocess.call(['flake8', '.']) == 0
+        assert subprocess.call(
+            'pip install -r requirements.txt '
+            '--upgrade-strategy=only-if-needed',
+            shell=True, stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL) == 0
+        assert subprocess.call(['pytest'], stdout=subprocess.DEVNULL) == 0
 
-    @chdir(wkdir)
-    def test_new_cmd_curdir(self, runner):
-        assert os.getcwd() == self.wkdir
-
-        result = runner.invoke(
-            hobbit, [
-                '--echo', 'new', '-n', 'haha', '-f', '-d', '.',
-                '-p', '1024',
-            ], obj={})
-        assert result.exit_code == 0
-        assert os.path.exists(os.path.join(
-            os.getcwd(), 'app', 'models', '__init__.py'))
-        assert 'example.py' not in result.output
-
-    @chdir(wkdir)
-    def test_new_cmd(self, runner):
-        assert os.getcwd() == self.wkdir
-
-        cmd = [
-            '--echo', 'new', '-n', 'haha', '-p', '1024']
-        result = runner.invoke(hobbit, cmd, obj={})
-        # start + 29 files + 11 dir + 1 end + empty
-        # in this test case. main dir exists, so mkdir - 1
-        assert len(result.output.split('\n')) == 1 + 29 + 11 + 1 + 1 - 1
-        assert result.exit_code == 0
-
-        # test no -f worked
-        result = runner.invoke(hobbit, cmd, obj={})
-        assert result.exit_code == 0
-        assert ', ignore' in result.output
-        assert call(['flake8', '.']) == 0
-
-    @chdir(wkdir)
-    def test_new_cmd_celery(self, runner):
-        assert os.getcwd() == self.wkdir
-
-        cmd = [
-            '--echo', 'new', '-n', 'haha', '-p', '1024',
-            '--celery']
-        result = runner.invoke(hobbit, cmd, obj={})
-        # start + files + mkdir + tail
-        assert len(result.output.split('\n')) == 1 + 30 + 12 + 1
-        assert result.exit_code == 0
-        assert '/tasks' in result.output
-        assert call(['flake8', '.']) == 0
+        # test --force option
+        result = runner.invoke(hobbit, options, obj={})
+        assert all([i in result.output for i in ['exists ', 'ignore ...']])
+        options.extend(['-f'])
+        result = runner.invoke(hobbit, options, obj={})
+        assert any([i in result.output for i in ['exists ', 'ignore ...']])
 
     @pytest.fixture
     def csv_file(self, runner):
@@ -138,6 +110,7 @@ class TestHobbit(BaseTest):
         assert len(result.output.split('\n')) == 5 + 1, result.output  # files
 
         # flake8 check
-        assert call(['flake8', '.']) == 0
+        assert subprocess.call(['flake8', '.']) == 0
         # pytest
-        assert call(['pytest']) == 0
+        assert subprocess.call(
+            ['pytest', '--no-cov'], stdout=subprocess.DEVNULL) == 0
