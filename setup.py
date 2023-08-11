bf762ae0982ed5ec19de3f556460f647d16bf8d0
legolas.zhan <legolas.zhan@webeye.cn>
2023-08-11 11:43:17 +0800

    release

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except (OSError, ImportError):
 
 setup(
     name='hobbit-core',
-    version='3.1.0',
+    version='3.1.1',
     python_requires='>=3.7, <4',
     description='Hobbit - A flask project generator.',
     long_description=long_description,
